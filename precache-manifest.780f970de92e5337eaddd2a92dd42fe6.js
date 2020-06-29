2b80c30e4774210666a22dc061d626f1377d0283
andreibuntsev <andrew.buntsev@gmail.com>
2020-06-29 18:22:39 +1000

    Updates

---
+++ precache-manifest.780f970de92e5337eaddd2a92dd42fe6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f70ceeb65ff8902dcf3a7df18baa7844",
+    "url": "/images-browser/index.html"
+  },
+  {
+    "revision": "5002559fcc1ae3b5c3cc",
+    "url": "/images-browser/static/css/main.a20e08be.chunk.css"
+  },
+  {
+    "revision": "95b200fddbf03dad6ed9",
+    "url": "/images-browser/static/js/2.f979ece9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/images-browser/static/js/2.f979ece9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5002559fcc1ae3b5c3cc",
+    "url": "/images-browser/static/js/main.3a5c5767.chunk.js"
+  },
+  {
+    "revision": "d9a3eab61c9cb397e5fa",
+    "url": "/images-browser/static/js/runtime-main.ca50e51b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/images-browser/static/media/logo.5d5d9eef.svg"
+  }
+]);
